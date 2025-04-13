2411becd83fa4e500078e5de525fb0510b77c6fa
Moonshammy <benjaminmooneyham1@gmail.com>
2025-04-13 06:40:23 -0600

    Removed bubble, added grids

--- main.c
+++ main.c
@@ -2,33 +2,32 @@
 #include <time.h>
 #include <stdlib.h>
 #include "engine.h"
-#include "bubble.h"
+#include "editor.h"
 
 const int WINDOW_WIDTH = 800;
 const int WINDOW_HEIGHT = 600;
+const int MAX_BUBBLES = 100;
+
 
 int main(int argc, char *argv[]) {
-    if (!engine_init("My Engine", WINDOW_WIDTH, WINDOW_HEIGHT)) {
+    if (!engine_init("Editor", WINDOW_WIDTH, WINDOW_HEIGHT)) {
         return 1;
     }
 
-    Bubble bubble;
-    srand((unsigned int)time(NULL));
-    bubble_init(&bubble, 400.0f, 300.0f, 250.0f, 25.0f);
+    editor_init(20,25,32);
 
     while (engine_is_running()) {
         engine_update();
 
         float dt = get_engine_delta_time();
-        bubble_update(&bubble, dt, WINDOW_WIDTH, WINDOW_HEIGHT);
+        editor_handle_input();
+        editor_update(dt);
 
         SDL_Renderer *renderer = get_engine_renderer();
-
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Black background
         SDL_RenderClear(renderer);
 
-        bubble_render(renderer, &bubble);
-
+        editor_render();
         SDL_Delay(16); // Roughly 60 FPS
         SDL_RenderPresent(renderer);
     }
